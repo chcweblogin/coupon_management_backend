f73e416b62c293e57545d9e364cca559f7d5d391
chcweblogin <chcweblogin@gmail.com>
2025-01-07 01:07:00 +0530

    gift product issue resolve

--- coupon_management_backend/settings.py
+++ coupon_management_backend/settings.py
@@ -113,6 +113,17 @@ DATABASES = {
    }
 }
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'coupon_management_db',
+#        'USER': 'postgres',
+#        'PASSWORD': 'postgres',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5433',
+#    }
+# }
+
 
 # DATABASES = {
 #     'default': {
